35942bbfd16773c277de11e0fba3cd4995eaed74
Anunay-Anand <anunayanand@gmail.com>
2021-05-28 03:20:41 +0530

    Added footer partials file. Added indexes to seperate comment. Added redirection for first time

--- index.js
+++ index.js
@@ -16,18 +16,22 @@ app.set('view engine', 'ejs');
 
 //Creating our comment resource
 const comments = [{
+        id: 1,
         username: 'Todd',
         comment: 'lol that is so funny'
     },
     {
+        id: 2,
         username: 'Skyler',
         comment: 'we will fly in the sky'
     },
     {
+        id: 3,
         username: 'Anunay',
         comment: 'Thak gya hu Bro!'
     },
     {
+        id: 4,
         username: 'coolDude69',
         comment: 'Mai sirf tumse baat karta hu!'
     },
@@ -40,21 +44,41 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
+//Index or home route
 app.get('/comments', (req, res) => {
     res.render('comments/index.ejs', {
         comments: comments
     });
 });
 
+//Part of Create Route
 app.get('/comments/new', (req, res) => {
     res.render('comments/new.ejs');
 });
 
+//Create Route
 app.post('/comments', (req, res) => {
-    const {username, comment} = req.body;
+    const {
+        username,
+        comment
+    } = req.body;
     //Update your comment database before rendering
-    comments.push({username: username, comment: comment});
-    res.send('It worked');
+    comments.push({
+        username: username,
+        comment: comment
+    });
+    //redirect user to the home or index url
+    res.redirect('/comments');
+});
+
+//Show route
+app.get('/comments/:id', (req, res) => {
+    const {
+        id
+    } = req.params;
+    //Find the comment in the database using ID
+    const comment = comments.find( c => c.id === parseInt(id));
+    
 });
 
 // app.get('/tacos', (req, res) => {
